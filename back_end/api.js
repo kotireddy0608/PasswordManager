7bed041c217fc6a5e1a2108afa76835d7af5b09b
pr1sm8 <saitherockstarnikith@gmail.com>
2022-05-19 13:10:47 +0530

    login api is working properly

--- back_end/api.js
+++ back_end/api.js
@@ -1,44 +1,57 @@
 'use strict';
-
+// /api
 const express = require('express');
 var router = express.Router();
-const mongo = require('mongodb');
+const {MongoClient} = require('mongodb');
 const jose = require('jose');
+const crypto = require('crypto');
+const hash = crypto.createHash('sha256');
 
-
-const uri = 'mongodb://localhost:27017/';
-async function dbconnection(){
-	const client = new mongo.MongoClient(uri);
-	try{
-		await client.connect();
-		let db = await client.db('passwordManager').command({ping : 1});
-		console.log("mongo successfully connected");
-		return db;
-	}finally{
-		await client.close();
-	}
-}
-
-const db = dbconnection().catch(console.error);
+//env variables
+const uri = process.env.MONGOURI || 'mongodb://localhost:27017/';
+const JWT_SECRET = crypto.createSecretKey(process.env.JWT_SECRET || 'pandemic-definite-galore-scion' , 'utf-8');
+const JWT_EXPIRATIONTIME = process.env.JWT_EXPIRATIONTIME || '1h';
+const client = MongoClient.connect(uri);
 
 router.get("/" , (req , res)=>{
 	res.send("api endpoint :)");
 })
 router.post("/login" ,express.json(), async (req , res)=>{
-	let users = await db;
+	let db = (await client).db('passwordManager');
+	let users = db.collection('users');
 	
-	users = users.collection('users');
-	let user = users.findOne({username : req.body.username});
+	let user = await users.findOne({"username":req.body.username});
+	
 	if(user){
-		if(user.password === req.body.password){
-			res.send("login successful");
+		const hash = crypto.createHash('sha256').update(req.body.password).digest('hex');
+		if(hash === user.password){
+			let token = await new jose.SignJWT({
+				username:user.username,
+				password:user.password
+			}).setExpirationTime(JWT_EXPIRATIONTIME)
+			.setProtectedHeader({
+				alg: 'HS256',
+			})
+			.sign(JWT_SECRET);
+			res.json({
+				status:"success",
+				token:token
+			});
 		}else{
-			res.send("wrong password");
+			res.status(401).json({
+				status:"reject"
+			});
 		}
+	}else{
+		res.status(401).json({
+			"status":"reject"
+		});
 	}
+	
+	
 })
 router.post("/signup" ,express.json(), (req , res)=>{
-	console.log(req.body);
+	console.log('signup request '+ req.body);
 	res.send(req.body);
 })
 module.exports = router;
