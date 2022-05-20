4308c34560ad78b5fd60467c263268a7b7fc7bdb
pr1sm8 <saitherockstarnikith@gmail.com>
2022-05-20 14:52:17 +0530

    added documentation to apis and better logging for the request processing

--- back_end/api.js
+++ back_end/api.js
@@ -5,7 +5,6 @@ var router = express.Router();
 const {MongoClient} = require('mongodb');
 const jose = require('jose');
 const crypto = require('crypto');
-const hash = crypto.createHash('sha256');
 
 //env variables
 const uri = process.env.MONGOURI || 'mongodb://localhost:27017/';
@@ -13,54 +12,23 @@ const JWT_SECRET = crypto.createSecretKey(process.env.JWT_SECRET || 'pandemic-de
 const JWT_EXPIRATIONTIME = process.env.JWT_EXPIRATIONTIME || '1h';
 const client = MongoClient.connect(uri);
 
+// a simple ping location to get if the server is up and running
 router.get("/" , (req , res)=>{
 	res.send("api endpoint :)");
 })
-router.post("/login" ,express.json(), async (req , res)=>{
-	let db = (await client).db('passwordManager');
-	let users = db.collection('users');
-	
-	let user = await users.findOne({"username":req.body.username});
-	
-	if(user){
-		const hash = crypto.createHash('sha256').update(req.body.password).digest('hex');
-		if(hash === user.password){
-			let token = await new jose.SignJWT({
-				username:user.username,
-				password:user.password
-			}).setExpirationTime(JWT_EXPIRATIONTIME)
-			.setProtectedHeader({
-				alg: 'HS256',
-			})
-			.sign(JWT_SECRET);
-			res.json({
-				status:"success",
-				token:token
-			});
-		}else{
-			res.status(401).json({
-				status:"reject"
-			});
-		}
-	}else{
-		res.status(401).json({
-			"status":"reject"
-		});
-	}
-	
-	
-})
+
+//client side validation is required
+/*
+schema
+username : string
+password : string
+Name : string
+email : string
+password_hint : string
+password_hint_answer : string
+*/
+// expected behaviour : return a json with status and message for the passed login credentials
 router.post("/signup" ,express.json(), async (req , res)=>{
-	//client side validation is required
-	/*
-	schema
-	username : string
-	password : string
-	Name : string
-	email : string
-	password_hint : string
-	password_hint_answer : string
-	*/
 	console.log(req.body);
 	if(req.body.username && req.body.password && req.body.Name && req.body.email && req.body.password_hint && req.body.password_hint_answer){
 		let db = (await client).db('passwordManager');
@@ -85,4 +53,65 @@ router.post("/signup" ,express.json(), async (req , res)=>{
 			});
 	}
 })
+
+// api to generate valid authenmication tokens for the passed login credentials
+/*
+schema
+username : string
+password : string
+*/
+// expected behaviour : return a json with status and token for the passed login credentials
+router.post("/login" ,express.json(), async (req , res)=>{
+	console.log('login request from '+req.ip);
+	if(req.body.username && req.body.password){
+		
+		let db = (await client).db('passwordManager');
+		let users = db.collection('users');
+		
+		let user = await users.findOne({"username":req.body.username});
+		
+		if(user){
+			const hash = crypto.createHash('sha256').update(req.body.password).digest('hex');
+			if(hash === user.password){
+				let token = await new jose.SignJWT({
+					username:user.username,
+					password:user.password
+				})
+				.setIssuedAt()
+				.setExpirationTime(JWT_EXPIRATIONTIME)
+				.setProtectedHeader({
+					alg: 'HS256',
+				})
+				.sign(JWT_SECRET);
+				console.log('login request is valid from ip '+req.ip+' for user '+user.username);
+				res.json({
+					status:"success",
+					token:token
+				});
+			}else{
+				console.log('login request is invalid from ip '+req.ip+' for user '+user.username + ' password mismatch');
+				res.status(401).json({
+					status:"reject"
+				});
+			}
+		}else{
+			console.log('login request is invalid from ip '+req.ip+' for user '+req.body.username + ' user not found');
+			res.status(401).json({
+				"status":"reject"
+			});
+		}
+	}else{
+		console.log('malformed request from ip '+req.ip);
+		res.status(400).json({
+			status : 'reject'
+		});
+	}
+	
+	
+})
+
+// get decrypted data for the user
+
+
+
 module.exports = router;
